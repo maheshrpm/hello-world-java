a9e67389183686b84dc29a6dd0c4a4b1392ee175
Maheshkumar <maheshkumar.p@y3technologies.com>
2018-10-17 14:48:47 +0800

    Chenge greeting msg

--- dockergithelloworld/src/main/java/org/pictolearn/docker/HelloWorldPing.java
+++ dockergithelloworld/src/main/java/org/pictolearn/docker/HelloWorldPing.java
@@ -8,8 +8,8 @@ package org.pictolearn.docker;
  */
 public class HelloWorldPing {
 	public static void main(String args[]) throws Exception{
-		for(int i = 0 ; i < 10; i++){
-			System.out.println("HI World Ping " + i );
+		for(int i = 0 ; i < 50; i++){
+			System.out.println("Maheshrpm Hello World Ping " + i );
 			Thread.sleep(1000);
 		}
 	}
